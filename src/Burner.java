b76121feb810ebafe9f660089b1bb0ac6f6451ca
alexrpollock <arpollock@mines.edu>
2018-09-06 11:15:16 -0600

    Commit of Burner and Stove class files.

---
+++ src/Burner.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+
+/**
+ * Burner class
+ * Used as part of the Stove assignment
+ * 
+ * @author Alex Pollock
+ * @author Jay Harrison
+ * @author Cassi VanDeventer
+ * 
+ */
+public class Burner {
+
+}
